34f2d4f9a924bce93071dc2c394fcea84f1737a0
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-02-10 15:43:05 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -516,7 +516,7 @@ class PlayState extends MusicBeatState
 
 	override public function create()
 	{
-	        if (['bf-shiny'].contains(boyfriend.curCharacter()) && FlxG.random.int(1, 4096) * 100)
+		if (boyfriend.curCharacter == 'bf-shiny' && FlxG.random.bool(1, 4096) * 100)
 	        {
 	             boyfriend.curCharacter == 'bf-shiny';
 	        }
